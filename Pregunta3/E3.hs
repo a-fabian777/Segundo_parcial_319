1bd7f1a83473280098bac0c7f62e516138c6cfc3
a_fabian_alcon <73066810+a-fabian777@users.noreply.github.com>
2022-11-30 18:52:14 -0400

    Update E3.hs

--- Pregunta3/E3.hs
+++ Pregunta3/E3.hs
@@ -1,1 +1,11 @@
-Ejercicio 3 en haskell
+-- Ejericicio 3 haskell
+-- lista subconjunto de otra
+
+subConj :: Eq a => [a] -> [a] -> Bool
+subConj [] [] = True
+subConj _ [] = False
+subConj [] _ = True
+
+subConj (x:xs) (y:ys)
+    | x == y = subConj xs ys
+    | otherwise = subConj (x:xs) ys

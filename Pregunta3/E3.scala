40628530a6c1d6bbc8fa5b7c0c6c943cfdc8573e
a_fabian_alcon <73066810+a-fabian777@users.noreply.github.com>
2022-12-01 10:32:10 -0600

    Update E3.scala

--- Pregunta3/E3.scala
+++ Pregunta3/E3.scala
@@ -1,13 +1,20 @@
-
-/*Ejercicio 3
+/*
+Ejercicio 3
 ver si una lista es subconjunto de otra
+scala
 */
 object Main {
-  def main(args: Array[String]): Unit = {
-    if(List(1, 2, 8).forall(List(1, 2, 3, 4).contains)){
+  def subconj(l1:List[Int], l2:List[Int]): Unit ={
+    if(l1.forall(l2.contains)){
       print("es subconjunto")
     }else{
       print("no es subcojunto")
     }
+  }
+  
+  def main(args: Array[String]): Unit = {
+    var l1 = List(1,5,8,7)
+    var l2 = List(1,5,2,8,6,7,4)
+    subconj(l1, l2)
   }
 }

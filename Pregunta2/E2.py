88054008c72786b30c443b27b25b2e60e0d9cbf0
a_fabian_alcon <73066810+a-fabian777@users.noreply.github.com>
2022-12-01 08:39:39 -0600

    Update E2.py

--- Pregunta2/E2.py
+++ Pregunta2/E2.py
@@ -1,1 +1,15 @@
-Ejercicio2 en python
+# Fibonnaci base 3 con funciones temporales
+
+suma3 = lambda a, b, c: a + b + c
+fibo = lambda n: fiboRec(n, -1, 1, 0)
+
+def fiboRec(n, a, b, c):
+    if n >= 0:
+        f = suma3(a, b, c)
+        print(f, end=" ")
+        fiboRec(n-1, b, c, f)
+
+n = int(input("Ingrese numero de terminos: "))
+print("fibonacci base 3 con funciones temporales")
+fibo(n)
+
